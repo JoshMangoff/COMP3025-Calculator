a0e3ff4c85e6b12e01298d8ae4fd8f9bbefea3b4
JoshMangoff <jmangoff@hotmail.com>
2017-01-31 11:48:11 -0500

    Calculations and Operators

--- Scripts/myjs/buttonjs.js
+++ Scripts/myjs/buttonjs.js
@@ -1,34 +1,72 @@
+/*
+* Josh Mangoff
+* Jan 30 2017
+* button events for calculator app
+*/
 $(document).bind('pageinit',function(){
 
+    //variables
     var invisibleNum = 0;
-    var operator = "=";
+    var operator = "";
     var nextNum = true;
 
+    //function for buttons
     $("button").bind("click",function(event, ui){
+        //variables reset every button click
         var visibleNum = document.getElementById("display").value;
         var pressedButton = $(this).text();
         
-        if (visibleNum == "AC") {
+        //if for what button was clicked
+        if (pressedButton == "AC") {
             nextNum = true;
             visibleNum = 0;
             invisibleNum = 0;
-            operator = "=";
+            operator = "";
             document.getElementById("display").value = visibleNum;
         }
-        else if (visibleNum == /*operators*/) {
-            if (operator != "=") {
-                //calcs
+        else if (pressedButton == "+" | pressedButton == "-"
+            | pressedButton == "*" | pressedButton == "/"
+            | pressedButton == "=") {
+
+            if (nextNum == false) {
+                if (operator == "+") {
+                    invisibleNum = invisibleNum + visibleNum;
+                    document.getElementById("display").value = invisibleNum;
+                }
+                else if (operator == "-") {
+                    invisibleNum = invisibleNum - visibleNum;
+                    document.getElementById("display").value = invisibleNum;
+                }
+                else if (operator == "*") {
+                    invisibleNum = invisibleNum * visibleNum;
+                    document.getElementById("display").value = invisibleNum;
+                }
+                else if (operator == "/") {
+                    invisibleNum = invisibleNum / visibleNum;
+                    document.getElementById("display").value = invisibleNum;
+                }
+                nextNum = true;
+                if (pressedButton == "=") {
+                    operator = pressedButton;
+                }
+                invisibleNum = visibleNum;
             }
-            nextNum = true;
-            invisibleNum = visibleNum;
+            if (pressedButton != "=") {
+                operator = pressedButton;
+            }
+
         }
         else {
-            if (nextNum) {
-                visibleNum = "";
-                nextNum = false;
+            if (operator != "=") {
+
+                if (nextNum) {
+                    visibleNum = "";
+                    nextNum = false;
+                }
+                document.getElementById("display").value 
+                    = visibleNum + pressedButton
+
             }
-            document.getElementById("display").value 
-                = visibleNum + pressedButton
         }
     });
 
