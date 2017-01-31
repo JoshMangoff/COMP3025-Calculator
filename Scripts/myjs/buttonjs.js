f43eb10119217b8f23fa80088884390976e56e2e
JoshMangoff <jmangoff@hotmail.com>
2017-01-31 15:23:05 -0500

    Error check and decimals

--- Scripts/myjs/buttonjs.js
+++ Scripts/myjs/buttonjs.js
@@ -25,57 +25,91 @@ $(document).bind('pageinit',function(){
             operator = "";
             document.getElementById("display").value = visibleNum;
         }
-        else if (pressedButton == "+" | pressedButton == "-"
-            | pressedButton == "*" | pressedButton == "/"
-            | pressedButton == "=") {
-            //operator and equals buttons
-            if (nextNum == false) {
-                if (operator == "+") {
-                    invisibleNum = parseFloat(invisibleNum)
-                         + parseFloat(visibleNum);
-                    document.getElementById("display").value = invisibleNum;
-                }
-                else if (operator == "-") {
-                    invisibleNum = parseFloat(invisibleNum)
-                         - parseFloat(visibleNum);
-                    document.getElementById("display").value = invisibleNum;
-                }
-                else if (operator == "*") {
-                    invisibleNum = invisibleNum * visibleNum;
-                    document.getElementById("display").value = invisibleNum;
-                }
-                else if (operator == "/") {
-                    invisibleNum = invisibleNum / visibleNum;
-                    document.getElementById("display").value = invisibleNum;
-                }
+        else if (visibleNum != "ERORR")
+        {
 
-                //for avoiding invalid order of button clicks
-                if (pressedButton == "=") {
+            if (pressedButton == "+" | pressedButton == "-"
+                | pressedButton == "*" | pressedButton == "/"
+                | pressedButton == "=") {
+                //operator and equals buttons
+                if (nextNum == false) {
+                    if (operator == "+") {
+                        invisibleNum = parseFloat(invisibleNum)
+                            + parseFloat(visibleNum);
+                        document.getElementById("display").value = invisibleNum;
+                    }
+                    else if (operator == "-") {
+                        invisibleNum = parseFloat(invisibleNum)
+                            - parseFloat(visibleNum);
+                        document.getElementById("display").value = invisibleNum;
+                    }
+                    else if (operator == "*") {
+                        invisibleNum = invisibleNum * visibleNum;
+                        document.getElementById("display").value = invisibleNum;
+                    }
+                    else if (operator == "/") {
+                        if (visibleNum != 0) {
+                            invisibleNum = invisibleNum / visibleNum;
+                            document.getElementById("display").value = invisibleNum;
+                        }
+                        else {
+                            //divide by 0 errors
+                            document.getElementById("display").value = "ERORR";
+                        }
+                    }
+
+                    //for overflow errors
+                    
+
+                    //for avoiding invalid order of button clicks
+                    if (pressedButton == "=") {
+                        operator = pressedButton;
+                    }
+                    else if (operator == "") {
+                        invisibleNum = visibleNum;
+                    }
+                    
+                    nextNum = true;
+                }
+                if (pressedButton != "=") {
+                    //for avoiding invalid order of button clicks
                     operator = pressedButton;
                 }
-                else if (operator == "") {
-                    invisibleNum = visibleNum;
-                }
-                
-                nextNum = true;
-            }
-            if (pressedButton != "=") {
-                //for avoiding invalid order of button clicks
-                operator = pressedButton;
-            }
 
-        }
-        else {
-            if (operator != "=") {
+            }
+            else {
                 //for numbers and decimal
-                if (nextNum) {
-                    visibleNum = "";
-                    nextNum = false;
-                }
-                document.getElementById("display").value 
-                    = visibleNum + pressedButton
+                if (operator != "=") {
+                    if (nextNum) {
+                        visibleNum = "";
+                        nextNum = false;
+                    }
+                    if (visibleNum != 0 | pressedButton != ".") {
+                        //max length of calculator display
+                        if (visibleNum.indexOf(".") == -1)
+                        {
+                            if (visibleNum.length < 8) {
+                                document.getElementById("display").value 
+                                    = visibleNum + pressedButton;
+                            }
+                        }
+                        else {
+                            if (visibleNum.length < 9 & pressedButton != ".") {
+                                document.getElementById("display").value 
+                                    = visibleNum + pressedButton;
+                            }
+                        }
+                    }
+                    else {
+                        //for if label is 0 and user clicks 0 or .
+                        if (pressedButton == ".") {
+                            document.getElementById("display").value = 0 + pressedButton;
+                        }
+                    }
 
+                }
             }
+
         }
     });
 
